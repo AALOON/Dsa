9e5b08badae4ed1bc79c47daedaf5ccdf3b590df
AALOON <zabirov.albert@gmail.com>
2019-02-17 19:40:18 +0300

    added compare extensions

---
+++ Dsa.Collections/Extensions/ComparerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Dsa.Collections.Extensions
+{
+    /// <summary>
+    /// Help function for comparer
+    /// </summary>
+    public static class ComparerExtensions
+    {
+        public static bool IsLess<TElement>(
+            this TElement a, TElement b, IComparer<TElement> comparer)
+        {
+            return comparer.Compare(a, b) < 0;
+        }
+
+        public static bool IsMore<TElement>(
+            this TElement a, TElement b, IComparer<TElement> comparer)
+        {
+            return comparer.Compare(a, b) > 0;
+        }
+
+        public static bool IsEqual<TElement>(
+            this TElement a, TElement b, IComparer<TElement> comparer)
+        {
+            return comparer.Compare(a, b) == 0;
+        }
+    }
+}
---
+++ Dsa.Collections/ReverseComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Dsa.Collections
+{
+    public class ReverseComparer<T> : IComparer<T>
+    {
+        private readonly IComparer<T> _baseComaprer;
+
+        private ReverseComparer()
+            :this(Comparer<T>.Default)
+        {
+        }
+
+        private ReverseComparer(IComparer<T> baseComaprer)
+        {
+            _baseComaprer = baseComaprer;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return _baseComaprer.Compare(y, x);
+        }
+
+        /// <summary>
+        /// Returns a default reverser sort order comparer for the type specified by the generic argument.
+        /// </summary>
+        public static ReverseComparer<T> Default => new ReverseComparer<T>();
+    }
+}

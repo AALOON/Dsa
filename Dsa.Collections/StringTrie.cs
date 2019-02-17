0d4895c1443c79a4cac5fd7dbb97bd31692570fa
AALOON <zabirov.albert@gmail.com>
2019-02-17 19:41:37 +0300

    adde trie

---
+++ Dsa.Collections/StringTrie.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsa.Collections
+{
+    public class StringTrie : ICollection<string>
+    {
+        private class Node : IEnumerable<Node>
+        {
+            private const int ChildsCapacity = 1;
+            private readonly IDictionary<char, Node> _childs;
+
+            public Node(char value, IEqualityComparer<char> comparer)
+            {
+                Value = value;
+                _childs = new Dictionary<char, Node>(ChildsCapacity, comparer);
+            }
+
+            public Node(Node parent, char value, IEqualityComparer<char> comparer)
+            {
+                Value = value;
+                _childs = new Dictionary<char, Node>(ChildsCapacity, comparer);
+                Parent = parent;
+            }
+
+            public char Value { get; }
+
+            public Node Parent { get; }
+
+            public bool IsSequenceEnd { get; set; }
+
+            public bool Contains(char item)
+            {
+                return _childs.ContainsKey(item);
+            }
+
+            public bool Any()
+            {
+                return _childs.Count > 0;
+            }
+
+            public Node this[char key]
+            {
+                get => _childs[key];
+                set => _childs[key] = value;
+            }
+
+            public bool Remove(char key)
+            {
+                return _childs.Remove(key);
+            }
+
+            public static Node GetRoot(IEqualityComparer<char> comparer)
+            {
+                return new Node(default(char), comparer);
+            }
+
+            public IEnumerator<Node> GetEnumerator()
+            {
+                return _childs.Values.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                var current = this;
+
+                while (current != null)
+                {
+                    sb.Append(current.Value);
+                    current = current.Parent;
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        private readonly IEqualityComparer<char> _comparer;
+
+        private Node _root;
+
+        public StringTrie()
+        {
+            _comparer = EqualityComparer<char>.Default;
+            Clear();
+        }
+
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return GetEnumeratorInternal(_root);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(string sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+            {
+                if (!current.Contains(item))
+                    current[item] = new Node(current, item, _comparer);
+                current = current[item];
+            }
+
+            if (!current.IsSequenceEnd)
+            {
+                current.IsSequenceEnd = true;
+                Count++;
+            }
+        }
+
+        public void Clear()
+        {
+            _root = Node.GetRoot(_comparer);
+            Count = 0;
+        }
+
+        public bool Contains(string sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+                if (!current.Contains(item))
+                    return false;
+            return current.IsSequenceEnd;
+        }
+
+        public void CopyTo(string[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex),
+                    $"Wrong index [{arrayIndex}] need to be [{0}, {array.Length - 1}]!");
+            if (array.Length - arrayIndex < Count)
+                throw new ArgumentException("Wrong values of array and/or arrayIndex");
+
+            foreach (var str in this)
+            {
+                array[arrayIndex] = str;
+                arrayIndex++;
+            }
+        }
+
+        public bool Remove(string item)
+        {
+            var node = FindInternal(item);
+            if (node == null)
+                return false;
+
+            node.IsSequenceEnd = false;
+            var current = node;
+            while (current != null && !current.Any() && !current.IsSequenceEnd)
+            {
+                current.Parent?.Remove(current.Value);
+                current = current.Parent;
+            }
+            return true;
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        private Node FindInternal(string sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+                if (!current.Contains(item))
+                    return null;
+            if (current.IsSequenceEnd)
+                return current;
+            return null;
+        }
+
+
+        private IEnumerator<string> GetEnumeratorInternal(Node current)
+        {
+            foreach (var node in current)
+            {
+                if (node.IsSequenceEnd)
+                    yield return node.ToString();
+
+                using (var nodeEnumerator = GetEnumeratorInternal(node))
+                    if (nodeEnumerator.MoveNext())
+                        yield return nodeEnumerator.Current;
+            }
+        }
+    }
+}
---
+++ Dsa.Collections/Trie.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Dsa.Collections
+{
+    public class Trie<TSequence, TChar> : ICollection<TSequence> where TSequence : IEnumerable<TChar>
+    {
+        private class Node : IEnumerable<Node>
+        {
+            private const int ChildsCapacity = 1;
+            private readonly IDictionary<TChar, Node> _childs;
+
+            public Node(TChar value, IEqualityComparer<TChar> comparer)
+            {
+                Value = value;
+                _childs = new Dictionary<TChar, Node>(ChildsCapacity, comparer);
+            }
+
+            public Node(Node parent, TChar value, IEqualityComparer<TChar> comparer)
+            {
+                Value = value;
+                _childs = new Dictionary<TChar, Node>(ChildsCapacity, comparer);
+                Parent = parent;
+            }
+
+            public TChar Value { get; }
+
+            public Node Parent { get; }
+
+            public bool IsSequenceEnd { get; set; }
+
+            public bool Contains(TChar item)
+            {
+                return _childs.ContainsKey(item);
+            }
+
+            public bool Any()
+            {
+                return _childs.Count > 0;
+            }
+
+            public Node this[TChar key]
+            {
+                get => _childs[key];
+                set => _childs[key] = value;
+            }
+
+            public bool Remove(TChar key)
+            {
+                return _childs.Remove(key);
+            }
+
+            public static Node GetRoot(IEqualityComparer<TChar> comparer)
+            {
+                return new Node(default(TChar), comparer);
+            }
+
+            public IEnumerator<Node> GetEnumerator()
+            {
+                return _childs.Values.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        private readonly IEqualityComparer<TChar> _comparer;
+
+        private Node _root;
+
+        public Trie()
+        {
+            _comparer = EqualityComparer<TChar>.Default;
+            Clear();
+        }
+
+
+        public IEnumerator<TSequence> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(TSequence sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+            {
+                if (!current.Contains(item))
+                    current[item] = new Node(current, item, _comparer);
+                current = current[item];
+            }
+
+            if (!current.IsSequenceEnd)
+            {
+                current.IsSequenceEnd = true;
+                Count++;
+            }
+        }
+
+        public void Clear()
+        {
+            _root = Node.GetRoot(_comparer);
+            Count = 0;
+        }
+
+        public bool Contains(TSequence sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+                if (!current.Contains(item))
+                    return false;
+            return current.IsSequenceEnd;
+        }
+
+        public void CopyTo(TSequence[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TSequence item)
+        {
+            var node = FindInternal(item);
+            if (node == null)
+                return false;
+
+            node.IsSequenceEnd = false;
+            var current = node;
+            while (current != null && !current.Any() && !current.IsSequenceEnd)
+            {
+                current.Parent?.Remove(current.Value);
+                current = current.Parent;
+            }
+            return true;
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        private Node FindInternal(TSequence sequence)
+        {
+            var current = _root;
+            foreach (var item in sequence)
+                if (!current.Contains(item))
+                    return null;
+            if (current.IsSequenceEnd)
+                return current;
+            return null;
+        }
+    }
+}

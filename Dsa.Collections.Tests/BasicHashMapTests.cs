9ded356362ed3ac32047b138c324fcbae5554742
AALOON <zabirov.albert@gmail.com>
2019-02-17 19:42:00 +0300

    added few tests for heap and hashmap

---
+++ Dsa.Collections.Tests/BasicHashMapTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Dsa.Collections.Tests
+{
+    public class BasicHashMapTests
+    {
+        [Fact]
+        public void HashMap_IsEmpty()
+        {
+            var map = new HashMap<int, int>();
+            map.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void HashMapAddGet_Success()
+        {
+            const int key = 10;
+            const int value = 10;
+            var map = new HashMap<int, int>();
+            map.Add(key, value);
+            var val = map[key];
+            value.Should().Be(val);
+            map.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void HashMapGetSet_Success()
+        {
+            const int key = 10;
+            const int value = 10;
+            var map = new HashMap<int, int>();
+            map[key] = value;
+            var val = map[key];
+            value.Should().Be(val);
+        }
+
+        [Fact]
+        public void HashMapGetSet_Update_Success()
+        {
+            const int key = 10;
+            const int value = 10;
+            const int updatedValue = 50;
+            var map = new HashMap<int, int>();
+            map[key] = value;
+            map[key] = updatedValue;
+            var val = map[key];
+            updatedValue.Should().Be(val);
+        }
+
+        [Fact]
+        public void HashMapAddGet_Sequnce_Success()
+        {
+            const int count = 1000;
+            var map = new HashMap<int, int>();
+            for (int i = 0; i < count; i++)
+                map.Add(i, i + 10000);
+
+            map.Count.Should().Be(count);
+
+            for (int i = 0; i < count; i++)
+                map[i].Should().Be(i + 10000);
+        }
+
+        [Fact]
+        public void HashMapAddGet_RandomSequnce_Success()
+        {
+            const int count = 1000;
+            var totalCount = 0;
+            var map = new HashMap<int, int>();
+            var random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var next = random.Next();
+                if(map.ContainsKey(next))
+                    continue;
+                map.Add(next, next + 10000);
+                totalCount++;
+            }
+
+            map.Count.Should().Be(totalCount);
+
+            foreach (var key in map.Keys)
+                map[key].Should().Be(key + 10000);
+        }
+
+        [Fact]
+        public void HashMapAddRemove_Sequnce_Success()
+        {
+            const int count = 1000;
+            var map = new HashMap<int, int>();
+            for (int i = 0; i < count; i++)
+                map.Add(i, i + 10000);
+
+            map.Count.Should().Be(count);
+
+            for (int i = 0; i < count; i++)
+                map.Remove(i).Should().BeTrue();
+
+            map.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void HashMapAddRemove_Sequnce_Unsuccess()
+        {
+            const int count = 1000;
+            var map = new HashMap<int, int>();
+            for (int i = 0; i < count; i++)
+                map.Add(i, i + 10000);
+
+            map.Count.Should().Be(count);
+
+            for (int i = count; i < 2000; i++)
+                map.Remove(i).Should().BeFalse();
+
+            map.Count.Should().Be(count);
+        }
+
+        [Fact]
+        public void HashMapContainsKey_Sequnce_Success()
+        {
+            const int count = 1000;
+            var map = new HashMap<int, int>();
+            for (int i = 0; i < count; i++)
+                map.Add(i, i + 10000);
+
+            map.Count.Should().Be(count);
+
+            for (int i = 0; i < count; i++)
+                map.ContainsKey(i).Should().BeTrue();
+        }
+
+        [Fact]
+        public void HashMapContainsKey_Sequnce_Unsuccess()
+        {
+            const int count = 1000;
+            var map = new HashMap<int, int>();
+            for (int i = 0; i < count; i++)
+                map.Add(i, i + 10000);
+
+            map.Count.Should().Be(count);
+
+            for (int i = count; i < 2000; i++)
+                map.ContainsKey(i).Should().BeFalse();
+        }
+    }
+}
---
+++ Dsa.Collections.Tests/BasicHeapTests.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Dsa.Collections.Tests
+{
+    public class BasicHeapTests
+    {
+        [Fact]
+        public void Heap_IsEmpty()
+        {
+            var heap = new Heap<int>();
+            heap.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void HeapAdd_SuccessAddition()
+        {
+            const int value = 10;
+            var heap = new Heap<int>();
+            heap.Add(value);
+
+            heap.Count.Should().Be(1);
+            heap.Peek().Should().Be(value);
+        }
+
+        [Fact]
+        public void HeapAdd_SuccessThreeAddition()
+        {
+            const int value1 = 1;
+            const int value2 = 2;
+            const int value3 = 3;
+
+            var heap = new Heap<int>();
+
+            heap.Add(value1);
+            heap.Add(value2);
+
+            heap.Count.Should().Be(2);
+            heap.Peek().Should().Be(value1);
+
+            heap.Add(value3);
+            heap.Count.Should().Be(3);
+            heap.Peek().Should().Be(value1);
+            heap.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void HeapAdd_SuccessThreeReverseAddition()
+        {
+            const int value1 = 3;
+            const int value2 = 2;
+            const int value3 = 1;
+
+            var heap = new Heap<int>();
+
+            heap.Add(value1);
+            heap.Add(value2);
+
+            heap.Count.Should().Be(2);
+            heap.Peek().Should().Be(value2);
+
+            heap.Add(value3);
+            heap.Count.Should().Be(3);
+            heap.Peek().Should().Be(value3);
+            heap.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void HeapAdd_ReverseComparer_SuccessThreeAddition()
+        {
+            const int value1 = 1;
+            const int value2 = 2;
+            const int value3 = 3;
+
+            var heap = new Heap<int>(ReverseComparer<int>.Default);
+
+            heap.Add(value1);
+
+            heap.Add(value2);
+            heap.Count.Should().Be(2);
+            heap.Peek().Should().Be(value2);
+
+            heap.Add(value3);
+            heap.Count.Should().Be(3);
+            heap.Peek().Should().Be(value3);
+            heap.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void HeapAdd_ReverseComparer_SuccessThreeReverseAddition()
+        {
+            const int value1 = 3;
+            const int value2 = 2;
+            const int value3 = 1;
+
+            var heap = new Heap<int>(ReverseComparer<int>.Default);
+
+            heap.Add(value1);
+
+            heap.Add(value2);
+            heap.Count.Should().Be(2);
+            heap.Peek().Should().Be(value1);
+
+            heap.Add(value3);
+            heap.Count.Should().Be(3);
+            heap.Peek().Should().Be(value1);
+            heap.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void HeapAdd_CheckSequence_Success()
+        {
+            var heap = new Heap<int>();
+            for (var i = 0; i < 1000; i++)
+            {
+                heap.Add(i);
+                var peek = heap.Peek();
+                peek.Should().Be(0);
+            }
+            heap.Count.Should().Be(1000);
+        }
+
+        [Fact]
+        public void HeapAdd_CheckSequenceReverse_Success()
+        {
+            var heap = new Heap<int>();
+            for (var i = 1000; i < 0; i--)
+            {
+                heap.Add(i);
+                var peek = heap.Peek();
+                peek.Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void HeapPoll_CheckSequence_Success()
+        {
+            var heap = new Heap<int>();
+            for (var i = 0; i < 1000; i++)
+                heap.Add(i);
+
+            for (var i = 0; i < 1000; i++)
+            {
+                var peek = heap.Peek();
+                peek.Should().Be(i);
+                var poll = heap.Poll();
+                poll.Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void Heap_CheckRandomSequence_Success()
+        {
+            var heap = new Heap<int>();
+            var random = new Random();
+            for (var i = 0; i < 1000; i++)
+                heap.Add(random.Next());
+
+            var last = heap.Peek();
+            for (var i = 0; i < 1000; i++)
+            {
+                var peek = heap.Peek();
+                peek.Should().BeGreaterOrEqualTo(last);
+                var poll = heap.Poll();
+                poll.Should().BeGreaterOrEqualTo(last);
+                peek.Should().Be(poll);
+                last = peek;
+            }
+        }
+
+        [Fact]
+        public void Heap_CheckRandomSequenceUnique_Success()
+        {
+            var set = new HashSet<int>();
+            var heap = new Heap<int>();
+            var random = new Random();
+            for (var i = 0; i < 10000; i++)
+            {
+                var next = random.Next();
+                if (!set.Contains(next))
+                {
+                    heap.Add(next);
+                    set.Add(next);
+                }
+            }
+            heap.Count.Should().Be(set.Count);
+            var last = heap.Peek();
+            while (set.Count > 0)
+            {
+                var peek = heap.Peek();
+                peek.Should().BeGreaterOrEqualTo(last);
+                var poll = heap.Poll();
+                poll.Should().BeGreaterOrEqualTo(last);
+                peek.Should().Be(poll);
+                last = peek;
+                set.Remove(last).Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void Heap_CheckSequenceReverse_Success()
+        {
+            var heap = new Heap<int>();
+            for (var i = 10; i < 0; i++)
+            {
+                heap.Add(i);
+                var peek = heap.Peek();
+                peek.Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void Heap_ThreePop_Success()
+        {
+            const int value1 = 1;
+            const int value2 = 2;
+            const int value3 = 3;
+
+            var heap = new Heap<int> {value1, value2, value3};
+
+            var peek = heap.Peek();
+            peek.Should().Be(value1);
+            var pop = heap.Poll();
+            pop.Should().Be(value1);
+
+            peek = heap.Peek();
+            peek.Should().Be(value2);
+            pop = heap.Poll();
+            pop.Should().Be(value2);
+
+            peek = heap.Peek();
+            peek.Should().Be(value3);
+            pop = heap.Poll();
+            pop.Should().Be(value3);
+        }
+    }
+}
